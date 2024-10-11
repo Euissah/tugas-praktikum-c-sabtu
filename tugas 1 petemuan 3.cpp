5c010646bee9e66286157ba69769619a58a8e019
Euissah <euisaaaisaa@gmail.com>
2024-10-11 12:36:50 +0700

    Add files via upload

---
+++ tugas 1 petemuan 3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float alas, tinggi, luas, keliling;
+
+    
+    printf("Masukkan panjang alas segitiga (cm): ");
+    scanf("%f", &alas);
+    printf("Masukkan tinggi segitiga (cm): ");
+    scanf("%f", &tinggi);
+
+     sisiMiring = sqrt((alas / 2) * (alas / 2) + tinggi * tinggi);
+    keliling = alas + 2 * sisiMiring;
+
+   
+    printf("Luas segitiga: %.2f cm^2\n", luas);
+    printf("Keliling segitiga: %.2f cm\n", keliling);
+
+    return 0;
+}
