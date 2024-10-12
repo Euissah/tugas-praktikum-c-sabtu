6ad4b4c35000c297be709003ed44d44c4b2a2ff5
Euissah <euisaaaisaa@gmail.com>
2024-10-12 10:27:13 +0700

    Add files via upload

---
+++ Modul1/Tugas 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	
+    char nim[] = "2441013";
+    char nama[] = "Euis aisyah";
+    char jurusan[] = "Teknik Informatika";
+
+    printf("NIM: %s\n", nim);
+    printf("Nama: %s\n", nama);
+    printf("Jurusan: %s\n", jurusan);
+
+    return 0;
+}
